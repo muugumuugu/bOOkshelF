84847e612e1d5d2ecd702d428a3986dccc29e07c
muugumuugu <mogimogi120501@gmail.com>
2021-09-14 18:05:06 +0530

    scripts

---
+++ display.sh
@@ -0,0 +1,2 @@
+for f in *.epub ;  epub-thumbnailer "$f" "${f%epub}.png" 800; done;
+mogrify -thumbnail 200 -format webp -path thumbs *.png *.pdf[0] *.djvu[0]
---
+++ library.sh
@@ -0,0 +1,1 @@
+for f in */; do cd "$f" ; bash ../shelver.sh ; cd ../;done;
